367536c9e8c91646ed287c7356d9b0cc83061497
enzobenedetti <enzob210401@gmail.com>
2021-10-08 10:03:18 +0200

    Mob & Spawner script

---
+++ Assets/Script/Spawner.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject mobPrefab;
+
+    [SerializeField] private float timeToSpawn = 2f;
+    
+    private float _timer;
+    public float Timer
+    {
+        get => _timer;
+        set
+        {
+            _timer = value;
+            if (_timer < timeToSpawn) return;
+            SpawnMob();
+            _timer = 0f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer += Time.deltaTime;
+    }
+
+    void SpawnMob()
+    {
+        Instantiate(mobPrefab, transform.position, Quaternion.identity);
+    }
+}
