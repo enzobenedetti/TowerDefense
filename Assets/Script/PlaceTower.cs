d5abc7abfa0922211f84ba14689a8de401f2930b
enzobenedetti <enzob210401@gmail.com>
2021-10-08 16:43:53 +0200

    Add PointSphere

--- Assets/Script/PlaceTower.cs
+++ Assets/Script/PlaceTower.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class PlaceTower : MonoBehaviour
 {
+    [SerializeField] private GameObject pointSpherePrefab;
     [SerializeField] private GameObject towerPrefab;
+    private GameObject pointShpere;
     private Camera cam;
     
     // Start is called before the first frame update
     void Start()
     {
         cam = Camera.main;
+        pointShpere = Instantiate(pointSpherePrefab);
     }
 
     // Update is called once per frame
@@ -29,6 +32,26 @@ public class PlaceTower : MonoBehaviour
                 }
             }
             
+        }
+        else
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity))
+            {
+                pointShpere.transform.position = hit.point;
+                if (hit.transform.tag == "TowerPlace")
+                {
+                    if (Money.Amount >= Tower.towerCost)
+                    {
+                        pointShpere.GetComponent<MeshRenderer>().material.color = Color.blue;
+                    }
+                    else
+                    {
+                        pointShpere.GetComponent<MeshRenderer>().material.color = Color.gray;
+                    }
+                }
+                else if (pointShpere) pointShpere.GetComponent<MeshRenderer>().material.color = Color.clear;
+            }
         }
     }
 }
